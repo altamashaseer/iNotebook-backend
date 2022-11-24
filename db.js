95c418bc73806570b9acd4a35c8cb505afeaf16b
Altamash <altamashaseer9@gmail.com>
2022-11-25 02:07:56 +0530

    Mongo Atlas

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose'); 
-const mongoURI = "mongodb://localhost:27017/inotebook"
+const DB = 'mongodb+srv://altamashaseer:aseer$$$123@cluster0.kgfvrbl.mongodb.net/?retryWrites=true&w=majority' 
+
+// const mongoURI = "mongodb://localhost:27017/inotebook"
 
 const connectToMongo =()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("connected to Mongo");
-    })
+    mongoose.connect(DB).then(()=>{
+        console.log("connected to Mongo Atlas");
+    }).catch((err)=>console.log('Connection unsuccessful'))
 }
 
 module.exports = connectToMongo;
